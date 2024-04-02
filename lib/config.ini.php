f9a14a50480e7e4740872b136c58a308b4f78626
SANTISOFT <santimasoft@gmail.com>
2024-04-02 18:00:37 +0200

    first commit

---
+++ lib/config.ini.php
@@ -0,0 +1,31 @@
+<?php 
+	/** 
+	* Configuration
+
+	* @package Wojo Framework
+	* @author wojoscripts.com
+	* @copyright 2024
+	* @version Id: config.ini.php, v1.00 2024-03-28 05:12:41 gewa Exp $
+	*/
+ 
+	 if (!defined("_WOJO")) 
+     die('Direct access to this location is not allowed.');
+ 
+	/** 
+	* Database Constants - these constants refer to 
+	* the database configuration settings. 
+	*/
+	 define('DB_SERVER', 'localhost'); 
+	 define('DB_USER', 'u416716127_car'); 
+	 define('DB_PASS', 'Kigali123,'); 
+	 define('DB_DATABASE', 'u416716127_car');
+	 define('DB_DRIVER', 'mysql');
+ 
+	 define('INSTALL_KEY', 'SmNNRORbF32cFMLK'); 
+ 
+	/** 
+	* Show Debugger Console. 
+	* Display errors in console view. Not recommended for live site. true/false 
+	*/
+	 define('DEBUG', false);
+?>
